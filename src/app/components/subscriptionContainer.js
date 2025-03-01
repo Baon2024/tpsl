33c52e8f9ab2b392f2534a76a449392e969cebee
Baon2024 <Baon2020@protonmail.com>
2025-03-01 19:37:16 +0000

    added fix for redirect if user wants to subscribe but not signed up yet

--- src/app/components/subscriptionContainer.js
+++ src/app/components/subscriptionContainer.js
@@ -74,65 +74,6 @@ const plans = [
     return userProfile?.subscribed === true; //if userprofile.subscribed is true, then it returns truthy, making disabled true
 }
 
-async function subscribeButtonHandler() {
-
-  const user = JSON.parse(localStorage.getItem('userTPSL'));
-  const userProfile = JSON.parse(localStorage.getItem('userTPSLProfile'));
-  console.log("user in subscribeToTPSL is:", user);
-
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  
-  const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-  if (userProfile.subscribed === true) {
-      return alert("already subscribed!");
-  } 
-
-  const email = user.email;
-  const userId = user.id;
-
-  console.log("userId and email are:", userId, email);
-
-
-
-  //need email and userId
-
-  //do you need to send user's id to backend?
-  //what else do you need to send??
-
-  //need to send PRICE_lookup_KEY to backend i think??
-  const lookup_key = "monthly_subscription";
-
-  const res = await fetch("/api/checkout-session", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ lookup_key: lookup_key, email: email, userId: userId }),
-    });
-  
-    
-    const session = await res.json();
-    console.log("session returned to frontend is:", session);
-
-    //store sessionId in user's profile databvase, so can be rteueved for managing sub
-    const sessionId = session.session.id;
-
-     
-const { data, error } = await supabase
-.from('profiles')
-.update({
-  sessionId: sessionId })
-.eq("id", userId)
-.select()
-
-console.log("sessionId save function:", { data, error});
-
-    const sessionUrl = session.session.url;
-    console.log("sessionUrl you're abiout to be redirect to is:", sessionUrl);
-    window.location.href = sessionUrl; // Redirect to Stripe Checkout
-  
-}
-
 
 
 export default function PricingPage(setSubscriptionModalBox, subscriptionModalBox) {
@@ -151,6 +92,69 @@ export default function PricingPage(setSubscriptionModalBox, subscriptionModalBo
   function signUpHandler() {
     console.log("now redirevtcing to logInSignUp page...");
     router.push('/logInSignUp');
+  }
+  
+  async function subscribeButtonHandler() {
+
+    const user = JSON.parse(localStorage.getItem('userTPSL'));
+    const userProfile = JSON.parse(localStorage.getItem('userTPSLProfile'));
+    console.log("user in subscribeToTPSL is:", user);
+  
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    
+    const supabase = createClient(supabaseUrl, supabaseAnonKey)
+  
+    if (userProfile?.subscribed === true) {
+        return alert("already subscribed!");
+    } 
+    if (!userProfile?.subscribed) {
+        router.push('/logInSignUp');
+        return
+    }
+  
+    const email = user.email;
+    const userId = user.id;
+  
+    console.log("userId and email are:", userId, email);
+  
+  
+  
+    //need email and userId
+  
+    //do you need to send user's id to backend?
+    //what else do you need to send??
+  
+    //need to send PRICE_lookup_KEY to backend i think??
+    const lookup_key = "monthly_subscription";
+  
+    const res = await fetch("/api/checkout-session", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ lookup_key: lookup_key, email: email, userId: userId }),
+      });
+    
+      
+      const session = await res.json();
+      console.log("session returned to frontend is:", session);
+  
+      //store sessionId in user's profile databvase, so can be rteueved for managing sub
+      const sessionId = session.session.id;
+  
+       
+  const { data, error } = await supabase
+  .from('profiles')
+  .update({
+    sessionId: sessionId })
+  .eq("id", userId)
+  .select()
+  
+  console.log("sessionId save function:", { data, error});
+  
+      const sessionUrl = session.session.url;
+      console.log("sessionUrl you're abiout to be redirect to is:", sessionUrl);
+      window.location.href = sessionUrl; // Redirect to Stripe Checkout
+    
   }
   
 
